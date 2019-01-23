f16e66283aa53acf88601977d8d495fc45b46360
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-24 00:45:11 +0300

    resme göre renk degismesi özelliği eklendi ve uygulama bitti

--- lib/pokemon_detail.dart
+++ lib/pokemon_detail.dart
@@ -1,49 +1,134 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pokedex/model/pokedex.dart';
+import 'package:palette_generator/palette_generator.dart';
 
-class PokemonDetail extends StatelessWidget {
+class PokemonDetail extends StatefulWidget {
   Pokemon pokemon;
 
   PokemonDetail({this.pokemon});
 
   @override
+  PokemonDetailState createState() {
+    return new PokemonDetailState();
+  }
+}
+
+
+class PokemonDetailState extends State<PokemonDetail> {
+
+  PaletteGenerator paletteGenerator;
+  Color baskinRenk;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    baskinRenk=Colors.orangeAccent;
+    baskinRengiBul();
+  }
+
+  void baskinRengiBul()  {
+    Future<PaletteGenerator> fPaletGenerator =  PaletteGenerator.fromImageProvider(NetworkImage(widget.pokemon.img));
+    fPaletGenerator.then((value){
+      paletteGenerator = value;
+     // debugPrint("secilen renk :" + paletteGenerator.dominantColor.color.toString());
+
+      if(paletteGenerator != null && paletteGenerator.vibrantColor != null){
+          baskinRenk = paletteGenerator.vibrantColor.color;
+          setState(() {
+
+          });
+      }else if(paletteGenerator != null && paletteGenerator.dominantColor != null){
+        baskinRenk = paletteGenerator.dominantColor.color;
+        setState(() {
+
+        });
+      }else{
+        debugPrint("NULL COLOR");
+      }
+
+    });
+  }
+
+  @override
+  void dispose() {
+
+    super.dispose();
+  }
+
+
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      backgroundColor: Colors.deepOrange,
+      backgroundColor: baskinRenk,
       appBar: AppBar(
         elevation: 0,
+        backgroundColor: baskinRenk,
         title: Text(
-          pokemon.name,
+          widget.pokemon.name,
           textAlign: TextAlign.center,
         ),
       ),
-      body: Stack(
+      body: OrientationBuilder(builder: (context, orientation) {
+        if (orientation == Orientation.portrait) {
+          return dikeyBody(context);
+        } else {
+          return yatayBody(context);
+        }
+      }),
+    );
+  }
+
+  Widget yatayBody(BuildContext context) {
+    return Container(
+      width: MediaQuery
+          .of(context)
+          .size
+          .width - 30,
+      height: MediaQuery
+          .of(context)
+          .size
+          .height * (3 / 4),
+      margin: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.white),
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.white
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          Positioned(
-            height: MediaQuery.of(context).size.height * (7 / 10),
-            width: MediaQuery.of(context).size.width - 20,
-            left: 10,
-            top: MediaQuery.of(context).size.height * 0.1,
-            child: Card(
-              elevation: 6,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(14),
-              ),
+          Expanded(
+            flex: 2,
+            child: Hero(
+                tag: widget.pokemon.img,
+                child: Container(
+                  width: 200,
+                  child: Image.network(
+                    widget.pokemon.img,
+                    fit: BoxFit.fill,
+                  ),
+                )),
+          ),
+          Expanded(
+            flex: 4,
+            child: SingleChildScrollView(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   SizedBox(
                     height: 50,
                   ),
                   Text(
-                    pokemon.name,
+                    widget.pokemon.name,
                     style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                   ),
                   Text(
-                    "Height : " + pokemon.height,
+                    "Height : " + widget.pokemon.height,
                   ),
                   Text(
-                    "Weight : " + pokemon.weight,
+                    "Weight : " + widget.pokemon.weight,
                   ),
                   Text(
                     "Types",
@@ -51,8 +136,9 @@ class PokemonDetail extends StatelessWidget {
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: pokemon.type
-                        .map((tip) => Chip(
+                    children: widget.pokemon.type
+                        .map((tip) =>
+                        Chip(
                             backgroundColor: Colors.deepOrange.shade300,
                             label: Text(
                               tip,
@@ -68,30 +154,32 @@ class PokemonDetail extends StatelessWidget {
 
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: pokemon.prevEvolution != null ?
-                    pokemon.prevEvolution.map((evolution) => Chip(
-                        backgroundColor: Colors.deepOrange.shade300,
-                        label: Text(
-                          evolution.name,
-                          style: TextStyle(color: Colors.white),
-                        )))
+                    children: widget.pokemon.prevEvolution != null ?
+                    widget.pokemon.prevEvolution.map((evolution) =>
+                        Chip(
+                            backgroundColor: Colors.deepOrange.shade300,
+                            label: Text(
+                              evolution.name,
+                              style: TextStyle(color: Colors.white),
+                            )))
                         .toList() : [Text("İlk hali")],
                   ),
 
                   Text(
                     "Next Evolution",
-                    style: TextStyle( fontWeight: FontWeight.bold),
+                    style: TextStyle(fontWeight: FontWeight.bold),
                   ),
 
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: pokemon.nextEvolution != null ?
-                      pokemon.nextEvolution.map((evolution) => Chip(
-                        backgroundColor: Colors.deepOrange.shade300,
-                        label: Text(
-                          evolution.name,
-                          style: TextStyle(color: Colors.white),
-                        )))
+                    children: widget.pokemon.nextEvolution != null ?
+                    widget.pokemon.nextEvolution.map((evolution) =>
+                        Chip(
+                            backgroundColor: Colors.deepOrange.shade300,
+                            label: Text(
+                              evolution.name,
+                              style: TextStyle(color: Colors.white),
+                            )))
                         .toList() : [Text("Son hali")],
                   ),
 
@@ -102,13 +190,134 @@ class PokemonDetail extends StatelessWidget {
 
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: pokemon.weaknesses != null ?
-                    pokemon.weaknesses.map((weakness) => Chip(
-                        backgroundColor: Colors.deepOrange.shade300,
-                        label: Text(
-                          weakness,
-                          style: TextStyle(color: Colors.white),
-                        )))
+                    children: widget.pokemon.weaknesses != null ?
+                    widget.pokemon.weaknesses.map((weakness) =>
+                        Chip(
+                            backgroundColor: Colors.deepOrange.shade300,
+                            label: Text(
+                              weakness,
+                              style: TextStyle(color: Colors.white),
+                            )))
+                        .toList() : [Text("Zayıflığı yok")],
+                  ),
+
+                ],
+              ),
+            ),
+          )
+        ],
+
+      ),
+    );
+  }
+
+  Stack dikeyBody(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Positioned(
+          height: MediaQuery
+              .of(context)
+              .size
+              .height * (8 / 10),
+          width: MediaQuery
+              .of(context)
+              .size
+              .width - 20,
+          left: 10,
+          top: MediaQuery
+              .of(context)
+              .size
+              .height * 0.06,
+          child: Card(
+            elevation: 6,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(14),
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  SizedBox(
+                    height: 90,
+                  ),
+                  Text(
+                    widget.pokemon.name,
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Height : " + widget.pokemon.height,
+                  ),
+                  Text(
+                    "Weight : " + widget.pokemon.weight,
+                  ),
+                  Text(
+                    "Types",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: widget.pokemon.type
+                        .map((tip) =>
+                        Chip(
+                            backgroundColor: Colors.deepOrange.shade300,
+                            label: Text(
+                              tip,
+                              style: TextStyle(color: Colors.white),
+                            )))
+                        .toList(),
+                  ),
+
+                  Text(
+                    "Pre Evolution",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: widget.pokemon.prevEvolution != null ?
+                    widget.pokemon.prevEvolution.map((evolution) =>
+                        Chip(
+                            backgroundColor: Colors.deepOrange.shade300,
+                            label: Text(
+                              evolution.name,
+                              style: TextStyle(color: Colors.white),
+                            )))
+                        .toList() : [Text("İlk hali")],
+                  ),
+
+                  Text(
+                    "Next Evolution",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: widget.pokemon.nextEvolution != null ?
+                    widget.pokemon.nextEvolution.map((evolution) =>
+                        Chip(
+                            backgroundColor: Colors.deepOrange.shade300,
+                            label: Text(
+                              evolution.name,
+                              style: TextStyle(color: Colors.white),
+                            )))
+                        .toList() : [Text("Son hali")],
+                  ),
+
+                  Text(
+                    "Weakness",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: widget.pokemon.weaknesses != null ?
+                    widget.pokemon.weaknesses.map((weakness) =>
+                        Chip(
+                            backgroundColor: Colors.deepOrange.shade300,
+                            label: Text(
+                              weakness,
+                              style: TextStyle(color: Colors.white),
+                            )))
                         .toList() : [Text("Zayıflığı yok")],
                   ),
 
@@ -116,21 +325,21 @@ class PokemonDetail extends StatelessWidget {
               ),
             ),
           ),
-          Align(
-            alignment: Alignment.topCenter,
-            child: Hero(
-                tag: pokemon.img,
-                child: Container(
-                  width: 150,
-                  height: 150,
-                  child: Image.network(
-                    pokemon.img,
-                    fit: BoxFit.contain,
-                  ),
-                )),
-          )
-        ],
-      ),
+        ),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Hero(
+              tag: widget.pokemon.img,
+              child: Container(
+                width: 150,
+                height: 130,
+                child: Image.network(
+                  widget.pokemon.img,
+                  fit: BoxFit.contain,
+                ),
+              )),
+        )
+      ],
     );
   }
 }
--- lib/pokemon_list.dart
+++ lib/pokemon_list.dart
@@ -10,8 +10,8 @@ class PokemonList extends StatefulWidget {
 }
 
 class _PokemonListState extends State<PokemonList> {
-
-  String url = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+  String url =
+      "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
   Pokedex pokedex;
   Future<Pokedex> veri;
 
@@ -29,95 +29,139 @@ class _PokemonListState extends State<PokemonList> {
     veri = pokemonlariGetir();
   }
 
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Pokedex"),),
-      body: OrientationBuilder(
-        builder: (context, orientation){
-
-          if(orientation==Orientation.portrait){
-            return FutureBuilder(future: veri,
+      appBar: AppBar(
+        title: Text("Pokedex"),
+      ),
+      body: Container(
+        child: OrientationBuilder(builder: (context, orientation) {
+          if (orientation == Orientation.portrait) {
+            return FutureBuilder(
+                future: veri,
                 builder: (context, AsyncSnapshot<Pokedex> gelenPokedex) {
                   if (gelenPokedex.connectionState == ConnectionState.waiting) {
-                    return Center(child: CircularProgressIndicator(),);
-                  } else if (gelenPokedex.connectionState == ConnectionState.done) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (gelenPokedex.connectionState ==
+                      ConnectionState.done) {
 //              return GridView.builder(
 //                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
 //                      crossAxisCount: 2), itemBuilder: (context, index) {
 //                return Text(gelenPokedex.data.pokemon[index].name);
 //              });
 
-                    return GridView.count(crossAxisCount: 2,children: gelenPokedex.data.pokemon.map((poke) {
-                      return InkWell(
-                        onTap: (){
-                          Navigator.of(context).push(MaterialPageRoute(builder: (context)=>PokemonDetail(pokemon: poke,)));
-                        },
-                        child: Hero(tag: poke.img, child: Card(
-                          elevation: 6,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Container(
-                                width: 200,
-                                height: 150,
-                                child: FadeInImage.assetNetwork(placeholder: "assets/loading.gif", image: poke.img,fit: BoxFit.contain,),
+                    return GridView.count(
+                      crossAxisCount: 2,
+
+                      children: gelenPokedex.data.pokemon.map((poke) {
+                        return InkWell(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => PokemonDetail(
+                                      pokemon: poke,
+                                    )));
+                          },
+                          child: Hero(
+                            tag: poke.img,
+                            child: Card(
+                              elevation: 4,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Expanded(
+                                    child: Container(
+                                      height: 120,
+                                      child: FadeInImage.assetNetwork(
+                                        placeholder: "assets/loading.gif",
+                                        image: poke.img,
+
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    poke.name,
+                                    style: TextStyle(
+                                        fontSize: 22,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ],
                               ),
-                              Text(poke.name, style: TextStyle(fontSize: 22, color: Colors.black, fontWeight: FontWeight.bold),)
-                            ],
+                            ),
                           ),
-                        )),
-                      );
-                    }).toList(),);
-
-
+                        );
+                      }).toList(),
+                    );
                   }
                 });
-
-          }else{
-            return FutureBuilder(future: veri,
+          } else {
+            return FutureBuilder(
+                future: veri,
                 builder: (context, AsyncSnapshot<Pokedex> gelenPokedex) {
                   if (gelenPokedex.connectionState == ConnectionState.waiting) {
-                    return Center(child: CircularProgressIndicator(),);
-                  } else if (gelenPokedex.connectionState == ConnectionState.done) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (gelenPokedex.connectionState ==
+                      ConnectionState.done) {
 //              return GridView.builder(
 //                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
 //                      crossAxisCount: 2), itemBuilder: (context, index) {
 //                return Text(gelenPokedex.data.pokemon[index].name);
 //              });
 
-                    return GridView.extent(maxCrossAxisExtent:300 ,children: gelenPokedex.data.pokemon.map((poke) {
-                      return InkWell(
-                        onTap: (){
-                          Navigator.of(context).push(MaterialPageRoute(builder: (context)=>PokemonDetail(pokemon: poke,)));
-                        },
-                        child: Hero(tag: poke.img, child: Card(
-                          elevation: 6,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Container(
-                                width: 200,
-                                height: 150,
-                                child: FadeInImage.assetNetwork(placeholder: "assets/loading.gif", image: poke.img,fit: BoxFit.contain,),
-                              ),
-                              Text(poke.name, style: TextStyle(fontSize: 22, color: Colors.black, fontWeight: FontWeight.bold),)
-                            ],
-                          ),
-                        )),
-                      );
-                    }).toList(),);
-
-
+                    return GridView.extent(
+                      maxCrossAxisExtent: 300,
+                      children: gelenPokedex.data.pokemon.map((poke) {
+                        return InkWell(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => PokemonDetail(
+                                      pokemon: poke,
+                                    )));
+                          },
+                          child: Hero(
+                              tag: poke.img,
+                              child: Card(
+                                elevation: 6,
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Container(
+                                      width: 200,
+                                      height: 150,
+                                      child: FadeInImage.assetNetwork(
+                                        placeholder: "assets/loading.gif",
+                                        image: poke.img,
+                                        fit: BoxFit.contain,
+                                      ),
+                                    ),
+                                    Text(
+                                      poke.name,
+                                      style: TextStyle(
+                                          fontSize: 22,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
+                                ),
+                              )),
+                        );
+                      }).toList(),
+                    );
                   }
                 });
-
-
           }
-
-
-        }
+        }),
       ),
     );
   }
